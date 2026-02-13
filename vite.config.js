886689988e34f9c456fadf2ef44bc95e4db376e0
veto <veto@body.body-piercing.com>
2026-02-13 18:55:18 +0700

    up: dist/assets/index-407e1644.js dist/assets/index-a78d4f2d.css dist/css/app.css dist/css/pico.css dist/index.html dist/vite.svg src/#main.js# src/js/log2textarea.js src/js/main.js src/public/img/README.md src/public/img/favicon.ico src/public/img/favicon.png src/public/img/icon.png src/public/img/landing.png src/public/img/logo.png src/public/img/screenshot.png src/public/img/source.png vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import path from "path";
 export default defineConfig({
   base: "./",
   root: path.resolve("./src"),
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true, // also necessary
+  },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
