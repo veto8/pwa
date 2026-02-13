e0437b0c6d4cab69c126700b7fc31c373ceaef62
veto <veto@body.body-piercing.com>
2026-02-13 18:36:19 +0700

    up: README.md node_modules/.vite/deps_temp_6c78da61/package.json node_modules/.vite/deps_temp_ff60c25d/package.json src/LICENSE src/css/app.css src/css/pico.css src/db.js src/favicon.ico src/favicon.png src/index.html src/js/log2textarea.js src/js/main.js src/main.js src/manifest.webmanifest vite.config.js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SQLite3 with OPFS and JavaScript
 
 SQLite3 on the web with pure JavaScript
+
+inspired from: git@github.com:veto8/hola-sqlite3.git
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-
+import path from "path";
 export default defineConfig({
   base: "./",
+  root: path.resolve("./src"),
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
