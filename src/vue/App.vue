30e9262a467aac30ae1ff4b7bc165989e9c5e4b4
veto <veto@lab.local>
2026-02-21 10:55:43 +0700

    up: src/vue/App.vue vite.config.js

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <div id="app">
     <Header />
     <main class="container">
       <router-view />
     </main>
-  </div>
 </template>
 
 <script>
--- vite.config.js
+++ vite.config.js
@@ -17,11 +17,6 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    rollupOptions: {
-      main: resolve(__dirname, "index.html"),
-      contact: resolve(__dirname, "contact.html"),
-      about: resolve(__dirname, "about.html"),
-    },
   },
   server: {
     headers: {
