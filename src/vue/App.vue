91c3c6bb5dc5438d5726841221b08dff1d2bc9ec
veto <veto@lab.local>
2026-02-21 14:23:39 +0700

    up: src/index.html src/vue/App.vue src/vue/Sqlite.vue

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -3,14 +3,16 @@
     <main class="container">
       <router-view />
     </main>
-    <Add_to_desktop />    
+    <Add_to_desktop />
+    <Sqlite />
 </template>
 
 <script>
 import Header from './Header.vue';
 import Add_to_desktop from './Add_to_desktop.vue';
+import Sqlite from './Sqlite.vue';
 export default {
-  components: { Header,Add_to_desktop }
+  components: { Header, Add_to_desktop, Sqlite }
 }
 </script>
 
---
+++ src/vue/Sqlite.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="sqlite">
+  <button id="btnIniciarBaseDeDatos">Start Database</button>
+  <button disabled id="btnInsertar">Insert Person</button>
+  <button disabled id="btnObtener">List Persons</button>  
+  <input disabled value="Parzibyte" id="nombre" type="text" placeholder="Nombre">
+  <input disabled value="2050-01-01" id="fechaNacimiento" type="date">
+  <div id="contenedorPersonas"></div>
+</div>
+</template>
+
