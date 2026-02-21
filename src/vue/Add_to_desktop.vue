ff5c0a0408bbc991dd3c0d6b230197ad93d2375e
veto <veto@lab.local>
2026-02-21 13:57:41 +0700

    up: src/index.html src/js/coi-serviceworker.min.js src/js/log2textarea.js src/js/log2textarea/index.js src/js/log2textarea/package.json src/main.js src/vue/Add_to_desktop.vue src/vue/App.vue

---
+++ src/vue/Add_to_desktop.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="add_to_desktop">
+  <br>
+  <br>    
+  <button id="install_pwa">Install PWA</button>
+  <button id="refresh_pwa">Refresh PWA</button>  
+  <br>
+  <br>    
+</div>
+</template>
+
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -3,12 +3,14 @@
     <main class="container">
       <router-view />
     </main>
+    <Add_to_desktop />    
 </template>
 
 <script>
-import Header from './Header.vue'
+import Header from './Header.vue';
+import Add_to_desktop from './Add_to_desktop.vue';
 export default {
-  components: { Header }
+  components: { Header,Add_to_desktop }
 }
 </script>
 
